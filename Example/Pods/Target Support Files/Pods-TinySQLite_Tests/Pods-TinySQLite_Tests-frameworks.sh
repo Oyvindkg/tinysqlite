2e33d77138c4c73a054e7be79156fa13a7781e00
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-01-04 21:46:22 +0100

    Some tests for DatabaseConnection

--- Example/Pods/Target Support Files/Pods-TinySQLite_Tests/Pods-TinySQLite_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-TinySQLite_Tests/Pods-TinySQLite_Tests-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-TinySQLite_Tests/TinySQLite.framework"
+  install_framework "Pods-TinySQLite_Tests/sqlite3.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-TinySQLite_Tests/TinySQLite.framework"
+  install_framework "Pods-TinySQLite_Tests/sqlite3.framework"
 fi
