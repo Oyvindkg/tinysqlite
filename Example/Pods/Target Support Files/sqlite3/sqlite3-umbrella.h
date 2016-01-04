192b42f36688191dcdf8b93a0ddeca1edd39808b
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-01-04 23:45:32 +0100

    Release 0.2.0

---
+++ Example/Pods/Target Support Files/sqlite3/sqlite3-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "sqlite3.h"
+#import "sqlite3ext.h"
+
+FOUNDATION_EXPORT double sqlite3VersionNumber;
+FOUNDATION_EXPORT const unsigned char sqlite3VersionString[];
+
