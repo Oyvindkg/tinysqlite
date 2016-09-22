d5fa5abc13efd954f83b98b01f12be8a2bfa733e
Øyvind Grimnes <oyvindkg@yahoo.com>
2016-09-22 15:08:11 +0200

    Created new project file

--- TinySQLite.podspec
+++ TinySQLite.podspec
@@ -27,10 +27,7 @@ Pod::Spec.new do |s|
   s.platform     = :ios, '8.0'
   s.requires_arc = true
 
-  s.source_files = 'Pod/Classes/**/*'
-  s.resource_bundles = {
-    'TinySQLite' => ['Pod/Assets/*.png']
-  }
+  s.source_files = 'TinySQLite/Classes/**/*'
 
   s.dependency 'sqlite3'
 end
---
+++ TinySQLite/TinySQLite.h
@@ -0,0 +1,19 @@
+//
+//  TinySQLite.h
+//  TinySQLite
+//
+//  Created by Øyvind Grimnes on 22/09/16.
+//  Copyright © 2016 Øyvind Grimnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TinySQLite.
+FOUNDATION_EXPORT double TinySQLiteVersionNumber;
+
+//! Project version string for TinySQLite.
+FOUNDATION_EXPORT const unsigned char TinySQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinySQLite/PublicHeader.h>
+
+
